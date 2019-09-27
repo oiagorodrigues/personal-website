63d8bc96d132fb9a94a8271f9114bd9ea169145a
Iago <yagomello16@gmail.com>
2019-09-27 20:18:55 -0300

    Added fixed layout | override App.vue

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="main-wrapper">
+    <DefaultLayout>
+      <transition name="fade" appear>
+        <main>
+          <router-view />
+        </main>
+      </transition>
+    </DefaultLayout>
+  </div>
+</template>
+
+<static-query>
+query App {
+  metadata {
+    siteName
+    siteDescription
+  }
+}
+</static-query>
+
+<script>
+export default {
+  name: "App",
+  metaInfo() {
+    return {
+      title: this.$static.metadata.siteName,
+      meta: [
+        {
+          key: "description",
+          name: "description",
+          content: this.$static.metadata.siteDescription
+        }
+      ]
+    };
+  },
+  beforeMount() {
+    // window.addEventListener("netlifyIdentity", () => {
+    //   if (window.netlifyIdentity) {
+    //     window.netlifyIdentity.on("init", user => {
+    //       if (!user) {
+    //         window.netlifyIdentity.on("login", () => {
+    //           document.location.href = "/admin/";
+    //         });
+    //       }
+    //     });
+    //   }
+    // });
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style>
+.fade-enter-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter {
+  opacity: 0;
+}
+</style>
