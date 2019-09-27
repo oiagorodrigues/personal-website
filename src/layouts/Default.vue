3d8cf987c307ed1ccd16baf031b820279a3daf58
Iago <yagomello16@gmail.com>
2019-09-27 20:17:17 -0300

    Added AppNav to DefaultLayout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
-    <slot/>
+    <AppNav></AppNav>
+    <main>
+      <slot />
+    </main>
   </div>
 </template>
+
+<script>
+import { AppNav } from "~/components";
+export default {
+  name: "DefaultLayout",
+  components: { AppNav }
+};
+</script>
 
 <static-query>
 query {
@@ -23,9 +25,10 @@ query {
 
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
   line-height: 1.5;
 }
 
@@ -34,17 +37,5 @@ body {
   margin: 0 auto;
   padding-left: 20px;
   padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
 }
 </style>
