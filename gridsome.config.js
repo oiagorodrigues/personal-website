4eefc3413b0602b93e26761d24871679a267ae18
Iago <yagomello16@gmail.com>
2019-09-27 19:00:26 -0300

    Added NetlifyCMS | Post Template

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,31 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: 'Iago Rodrigues | Frontend Developer',
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link'
+    }
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'posts/**/*.md',
+        typeName: 'Post'
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    }
+  ]
+};
---
+++ src/templates/Post.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>PostTemplate</div>
+</template>
+
+<script>
+export default {
+  name: "PostTemplate"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
